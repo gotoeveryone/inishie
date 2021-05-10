61d1a8089ee2e67d30d39abbb22c202518c794e2
Kazuki_Kamizuru <k.amago0721@gmail.com>
2021-05-10 21:52:59 +0900

    disable contextmenu

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import App from './App.svelte';
 
+document.oncontextmenu = () => {
+  alert("\t※※※注意※※※\n★★★右クリック禁止！★★★");
+  return false;
+};
+
 const app = new App({
   target: document.body,
 });
