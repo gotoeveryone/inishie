d208214c003d6c37c59b772eeb8a0baaa7d9151c
Kazuki_Kamizuru <k.amago0721@gmail.com>
2026-01-23 22:58:08 +0900

    feat: adjust service worker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,19 +14,6 @@ onMount(() => {
 		alert("\t※※※注意※※※\n★★★右クリック禁止！★★★");
 		return false;
 	};
-	// ServiceWorker の登録
-	if ("serviceWorker" in navigator) {
-		navigator.serviceWorker
-			.register("/serviceWorker.js")
-			.then((registration) => {
-				if (typeof registration.update == "function") {
-					registration.update();
-				}
-			})
-			.catch((error) => {
-				console.log("Error Log: " + error);
-			});
-	}
 });
 </script>
 
